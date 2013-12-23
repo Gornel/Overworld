6fba1c0cf10934bc1603bb3868d03f8e413ac3d9
veridiam <alex@judgementstudios.com>
2013-12-23 06:57:14 -0800

    updated for unity specific files

---
+++ Assets/Programming/Debug/drawMyHitBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawMyHitBox : MonoBehaviour {
+	
+	public Color color = Color.green;
+
+	// Use this for initialization
+	void OnDrawGizmos()
+	{
+		   // set to whatever color you want to represent
+		Gizmos.color = color;
+			
+		// we’re going to draw the gizmo in local space
+		Gizmos.matrix = transform.localToWorldMatrix;
+		
+		// draw a box collider based on its size
+		BoxCollider box = GetComponent<BoxCollider>();
+		Gizmos.DrawWireCube(box.center, box.size);
+	}
+}
---
+++ Assets/Programming/Debug/killgriswold.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killgriswold : MonoBehaviour {
+	public SpriteController griswold;
+	public SpriteAnimation death;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0))
+		{
+			griswold.BeginAnimation(death);
+		}
+	}
+}
